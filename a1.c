a888d91fb91c01e261c811972cdc711019a65f00
BeckHarper13 <beckharper13@gmail.com>
2024-01-30 11:25:28 -0800

    Added Error Cases

--- a1.c
+++ a1.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
 
+//A01362009
+//A01362704
+//A01215993
+//A01316524
 int checkAnagram(char ana1[], char ana2[], FILE *out){
-    char letters1[35];
+    
+    //fills letters1 and letters2 with zeros 
+    char letters1[100];
     for (int i = 0; i < sizeof(letters1); i ++){
         letters1[i] = '0';
     }
-    char letters2[35];
+    char letters2[100];
     for (int i = 0; i < sizeof(letters2); i ++){
         letters2[i] = '0';
     }
 
 
+    //copies letters only to letters1 and letters2 from ana1 and ana2
     for (int i = 0, j = 0; i < sizeof(letters1); i++){
 
         if (ana1[i] >= 'a' && ana1[i] <= 'z') {
@@ -54,6 +61,11 @@ int checkAnagram(char ana1[], char ana2[], FILE *out){
 
     }
 
+    if (letters1[0] == '0' || letters2[0] =='0'){
+        fputs("Error. String is full on non alphabetical characters only", out);
+        return 0;
+    }
+
     printf("\nLetters 1\n");
 
 
@@ -66,6 +78,8 @@ int checkAnagram(char ana1[], char ana2[], FILE *out){
         printf("%c", letters2[x]);
     }
     printf("\n");
+
+
 
     for (int position1 = 0; position1 < sizeof(letters1); position1++){
         for (int position2 = 0; position2 < sizeof(letters2); position2++){
@@ -94,7 +108,7 @@ int checkAnagram(char ana1[], char ana2[], FILE *out){
         if ((letters1[i] >= 'a' && letters1[i] <= 'z' ) || (letters2[i] >= 'a' && letters2[i] <= 'z' )){
             
             fputs("0! not anagram", out);
-            return -1;
+            return 0;
         }   
         
     }
@@ -106,12 +120,17 @@ int checkAnagram(char ana1[], char ana2[], FILE *out){
 int main(int argsc, char *argv[]){
     FILE *anagram;
     FILE *output;
-    char buffer[1000];
-    char buffer2[1000];
+    char buffer[100];
+    char buffer2[100];
     anagram = fopen(argv[1], "r");
     output = fopen(argv[2], "w");
     fgets(buffer, sizeof(buffer), anagram);
     fgets(buffer2, sizeof(buffer2), anagram);
+
+    if ((buffer[0] == '\n') || buffer2[1] == '\0'){
+        fputs("Error. One or both of the strings are NULL", output);
+        return 0;
+    }
     
 
     if (checkAnagram(buffer, buffer2, output) > 0){
